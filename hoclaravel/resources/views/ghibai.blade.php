4fc7cf2bae749f64a83a8201a4f8d90a7a1a476f
Mai <mai.ho25@student.passerellesnumeriques.org>
2024-01-30 20:37:28 +0700

    Mai complete Bai2

---
+++ hoclaravel/resources/views/ghibai.blade.php
@@ -0,0 +1,13 @@
+
+    Vòng đời của request(request lifecycle laravel)
+    chạy vào public ->index.php
+    chạy vào bootstrap-> app.php
+    chạy vào app/Http/Kernel.php-> serviceproviders
+    Register Service providers
+    Bootstrap Service providers
+    Router
+    Middleware
+    Controller/Action->View->client
+    or Controller/Action->client
+
+
