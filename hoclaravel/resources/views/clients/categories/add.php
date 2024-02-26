dfc66be39a28b06976b043be968b1dd5658d8a4e
Mai <mai.ho25@student.passerellesnumeriques.org>
2024-02-26 11:20:28 +0700

    complete Bai10

--- hoclaravel/resources/views/clients/categories/add.php
+++ hoclaravel/resources/views/clients/categories/add.php
@@ -2,7 +2,8 @@
 <form action="<?php  echo route('categories.add'); ?>" method="POST">
 
     <div>
-    <input type="text" nme="category_name" placeholder="Ten chuyen muc">
+    <input type="text" nme="category_name" placeholder="Ten chuyen muc
+    value="<?php echo $old('category_name'); ?>">
     </div>
     <button type="submit"> Them chuyen muc</button>
     <?php echo csrf_field(); ?>
