2bc4275a638ab2857e357e030e6020f4337390ab
Mai <mai.ho25@student.passerellesnumeriques.org>
2024-03-22 06:31:09 +0700

    Bai 28

--- hoclaravel/resources/views/home.blade.php
+++ hoclaravel/resources/views/home.blade.php
@@ -1,56 +1,125 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title') - unicode</title>
-    <style type="text/css" >
-        h1{
-            background-color: blueviolet;
-            color: aliceblue;
-        }
-    </style>
-</head>
-<body>
-    @extends('layouts.client')
-
-    @section('title')
-        <h1>TRang chu</h1>
-        @env('local')
-            <p>Moi truowng dev</p>
-            @else
-            <p>khong phai moi truowng dev</p>
-        @endenv
-            <!-- <x-alert type="danger"/> -->
-            <x-alert type="danger" content="dat hang thanh cong"/>    
-        @push('script')
-            <script>
-                console.log('push lan 2')
-            </script>
-        @endpush
-        
-        @endsection
-        @section('sidebar')
-        @parent 
-        <h3>home sidebar</h3>
-        @endsection
-        
-        @section('content')
-        <h1>TRang chu</h1>
-        <p><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Golden_tabby_and_white_kitten_n01.jpg/1200px-Golden_tabby_and_white_kitten_n01.jpg" alt=""></p>
-        <p><a href="{{ route('downLoadimage').'?image=https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Golden_tabby_and_white_kitten_n01.jpg/1200px-Golden_tabby_and_white_kitten_n01.jpg'}}" class="btn btn-success" >Down loasd anh</a></p>
-        <p><a href="{{ route('downLoadimage').'?image='.public_path('storage/image_65e9aa10d925b.jpg')}}" class="btn btn-success" >Down loasd anh</a></p>
-
-        @endsection
-
-    @section('js')
-    @endsection
-
-    @push('script')
-    <script>
-        console.log('push lan 1')
-    </script>
-    @endpush
+@extends('layouts/client')
+@section('main_content')
     
-</body>
-</html>
+
+<div class="rev-slider">
+    <div class="fullwidthbanner-container">
+                <div class="fullwidthbanner">
+                    <div class="bannercontainer" >
+                    <div class="banner" >
+                            <ul>
+                                <!-- THE FIRST SLIDE -->
+                                <li data-transition="boxfade" data-slotamount="20" class="active-revslide" style="width: 100%; height: 100%; overflow: hidden; z-index: 18; visibility: hidden; opacity: 0;">
+                                <div class="slotholder" style="width:100%;height:100%;" data-duration="undefined" data-zoomstart="undefined" data-zoomend="undefined" data-rotationstart="undefined" data-rotationend="undefined" data-ease="undefined" data-bgpositionend="undefined" data-bgposition="undefined" data-kenburns="undefined" data-easeme="undefined" data-bgfit="undefined" data-bgfitend="undefined" data-owidth="undefined" data-oheight="undefined">
+                                                <div class="tp-bgimg defaultimg" data-lazyload="undefined" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat" data-lazydone="undefined" src="/source/image/slide/banner1.jpg" data-src="/source/image/slide/banner1.jpg" style="background-color: rgba(0, 0, 0, 0); background-repeat: no-repeat; background-image: url('/source/image/slide/banner1.jpg'); background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit;">
+                                                </div>
+                                            </div>
+
+                            </li>
+                            <li data-transition="boxfade" data-slotamount="20" class="active-revslide" style="width: 100%; height: 100%; overflow: hidden; z-index: 18; visibility: hidden; opacity: 0;">
+                              <div class="slotholder" style="width:100%;height:100%;" data-duration="undefined" data-zoomstart="undefined" data-zoomend="undefined" data-rotationstart="undefined" data-rotationend="undefined" data-ease="undefined" data-bgpositionend="undefined" data-bgposition="undefined" data-kenburns="undefined" data-easeme="undefined" data-bgfit="undefined" data-bgfitend="undefined" data-owidth="undefined" data-oheight="undefined">
+                                            <div class="tp-bgimg defaultimg" data-lazyload="undefined" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat" data-lazydone="undefined" src="/source/image/slide/banner2.jpg" data-src="/source/image/slide/banne2.jpg" style="background-color: rgba(0, 0, 0, 0); background-repeat: no-repeat; background-image: url('/source/image/slide/banner2.jpg'); background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit;">
+                                        </div>
+                                        </div>
+
+                            <li data-transition="boxfade" data-slotamount="20" class="active-revslide" style="width: 100%; height: 100%; overflow: hidden; z-index: 18; visibility: hidden; opacity: 0;">
+                                <div class="slotholder" style="width:100%;height:100%;" data-duration="undefined" data-zoomstart="undefined" data-zoomend="undefined" data-rotationstart="undefined" data-rotationend="undefined" data-ease="undefined" data-bgpositionend="undefined" data-bgposition="undefined" data-kenburns="undefined" data-easeme="undefined" data-bgfit="undefined" data-bgfitend="undefined" data-owidth="undefined" data-oheight="undefined">
+                                                <div class="tp-bgimg defaultimg" data-lazyload="undefined" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat" data-lazydone="undefined" src="/source/image/slide/banner3.jpg" data-src="/source/image/slide/banner3.jpg" style="background-color: rgba(0, 0, 0, 0); background-repeat: no-repeat; background-image: url('/source/image/slide/banner3.jpg'); background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit;">
+                                            </div>
+                                        </div>
+
+                            </li>
+
+                            <li data-transition="boxfade" data-slotamount="20" class="active-revslide current-sr-slide-visible" style="width: 100%; height: 100%; overflow: hidden; visibility: inherit; opacity: 1; z-index: 20;">
+                                <div class="slotholder" style="width:100%;height:100%;" data-duration="undefined" data-zoomstart="undefined" data-zoomend="undefined" data-rotationstart="undefined" data-rotationend="undefined" data-ease="undefined" data-bgpositionend="undefined" data-bgposition="undefined" data-kenburns="undefined" data-easeme="undefined" data-bgfit="undefined" data-bgfitend="undefined" data-owidth="undefined" data-oheight="undefined">
+                                                <div class="tp-bgimg defaultimg" data-lazyload="undefined" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat" data-lazydone="undefined" src="/source/image/slide/banner1.jpg" data-src="/source/image/slide/banner1.jpg" style="background-color: rgba(0, 0, 0, 0); background-repeat: no-repeat; background-image: url('/source/image/slide/banner1.jpg'); background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit;">
+                                            </div>
+                                        </div>
+
+                            </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="tp-bannertimer"></div>
+                </div>
+            </div>
+            <!--slider-->
+</div>
+<div class="container">
+    <div id="content" class="space-top-none">
+        <div class="main-content">
+            <div class="space60">&nbsp;</div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="beta-products-list">
+                        <h4>New Products</h4>
+                        <div class="beta-products-details">
+                            <p class="pull-left">438 styles found</p>
+                            <div class="clearfix"></div>
+                        </div>
+
+                        <div class="row">
+                            @foreach($products as $product)
+                            <div class="col-sm-3">
+                                <div class="single-item">
+                                    <div class="single-item-header">
+                                        <a href="detail?id={{ $product->id }}"><img src="/source/image/product/{{$product->image}}" alt=""></a>
+                                    </div>
+                                    <div class="single-item-body">
+                                        <p class="single-item-title">{{$product->name}}</p>
+                                        <p class="single-item-price">
+                                            <span>{{$product->price}}</span>
+                                        </p>
+                                    </div>
+                                    <div class="single-item-caption">
+                                        <a class="add-to-cart pull-left" href="shopping_cart.html"><i class="fa fa-shopping-cart"></i></a>
+                                        <a class="beta-btn primary" href="product.html">Details <i class="fa fa-chevron-right"></i></a>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            @endforeach
+                        </div>
+                    </div> <!-- .beta-products-list -->
+                    <div class="space50">&nbsp;</div>
+    
+                    <div class="beta-products-list">
+                        <h4>Top Products</h4>
+                        <div class="beta-products-details">
+                            <p class="pull-left">438 styles found</p>
+                            <div class="clearfix"></div>
+                        </div>
+                        <div class="row">
+                            
+                        @foreach($allProducts as $product)
+                            <div class="col-sm-3">
+                                <div class="single-item">
+                                    <div class="single-item-header">
+                                        <a href="product.html"><img src="/source/image/product/{{$product->image}}" alt=""></a>
+                                    </div>
+                                    <div class="single-item-body">
+                                        <p class="single-item-title">{{$product->name}}</p>
+                                        <p class="single-item-price">
+                                            <span>{{$product->price}}</span>
+                                        </p>
+                                    </div>
+                                    <div class="single-item-caption">
+                                        <a class="add-to-cart pull-left" href="shopping_cart.html"><i class="fa fa-shopping-cart"></i></a>
+                                        <a class="beta-btn primary" href="product.html">Details <i class="fa fa-chevron-right"></i></a>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            @endforeach
+                        </div>
+                        <div class="space40">&nbsp;</div>
+                    </div> <!-- .beta-products-list -->
+                </div>
+            </div> <!-- end section with sidebar and main content -->
+
+
+        </div> <!-- .main-content -->
+    </div> <!-- #content -->
+</div> <!-- .
+    @endsection
