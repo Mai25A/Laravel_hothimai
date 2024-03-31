64dacb1bbd4f44246c1714b2b56bc9578fff3fb8
Mai25A <127266061+Mai25A@users.noreply.github.com>
2024-04-01 01:31:39 +0700

    Laravel bai35 (#29)
    * Bai31
    
    * fix conflict
    
    * Bai 35

--- hoclaravel/app/Http/Controllers/homeController.php
+++ hoclaravel/app/Http/Controllers/homeController.php
@@ -7,11 +7,22 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public $data = [];
-    //Action index()N
-    public function index(){ 
+    protected $product; // Remove the instantiation here
+
+    public function __construct()
+    {
+        $this->product = new Product(); // Move the instantiation to the constructor
+    }
+    //Action index()
+    public function index(Request $request){ 
         $products = Product::all()->where('new','=',1);
         $allProducts = Product::all();
-            
+        $infor = $request->input('search');
+        
+
+        if (isset($infor)) {
+            return $this->search($infor);
+        }    
         // dd($products);
         return view('home',compact('products','allProducts'));  
     }
@@ -19,97 +30,6 @@ class HomeController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    
-    //Action getNews()
-    public function getNews(){
-    return 'News list';
-    }
-    public function getCategories($id) { 
-        return 'Chuyên mục: '.$id; 
-    }
-    public function products(){
-        $this->data['title'] = 'san pham';
-        return view('clients.products',$this->data);
-    }
-    public function getAdd(){
-        $this->data['title'] = 'them san pham';
-    
-        return view('clients.blocks.add',$this->data);
-    }
-    public function postAdd(Request $request){
-        return $request.'hih';
-    }
-    public function putAdd(Request $request){
-        return $request.'hihe';
-    }
-    public function getArray(){
-        $content = [
-            'name'=>'Mai',
-            'description'=>'sinh  vueb cua PNv',
-            'session'=>'laravel'
-        ];  
-        return $content;
-    }
-    public function downLoad(Request $request){
-        if(!empty($request->image)){
-            $image = trim($request->image);
-            $fileName = 'image_'.uniqid().'.jpg'; 
-        }
-    }
-            // $fileName = basename($image);
-        /**
-         * Show the form for creating a new resource.
-         */
-        public function create()
-        {
-            //
-    
-            // return response()->streamDownload(function()use ($image){
-            //     $imageContent = file_get_contents($image);
-            //     echo $imageContent;
-            // },$filename);
-            // return response()->download($image, $fileName);
-            }
-        /**
-         * Store a newly created resource in storage.
-         */
-        public function store(Request $request)
-        {
-            //
-        }
-    
-        /**
-         * Display the specified resource.
-         */
-        public function show(string $id)
-        {
-            //
-        }
-    
-        /**
-         * Show the form for editing the specified resource.
-         */
-        public function edit(string $id)
-        {
-            //
-        }
-    
-        /**
-         * Update the specified resource in storage.
-         */
-        public function update(Request $request, string $id)
-        {
-            //
-        }
-    
-        /**
-         * Remove the specified resource from storage.
-         */
-        public function destroy(string $id)
-        {
-            //
-        }
-=======
     public function create()
     {
         //
@@ -148,12 +68,26 @@ class HomeController extends Controller
     }
 
     /**
-
      * Remove the specified resource from storage.
      */
     public function destroy(string $id)
     {
         //
     }
+    public function search($infor)
+    {
+        $query = $this->product->query();
+        $keywords = explode(' ', $infor);
 
-}
+        foreach ($keywords as $keyword) {
+            if (is_numeric($keyword)) {
+                $query->orWhereRaw("CAST(price AS UNSIGNED) = ?", [explode('.', $keyword)[0]]);
+            } else {
+                $query->orWhere('name', 'like', '%' . $keyword . '%')
+                    ->orWhere('weight', 'like', '%' . $keyword . '%')
+                    ->orWhere('rating', 'like', '%' . $keyword . '%')
+                    ->orWhere('size', 'like', '%' . $keyword . '%')
+                    ->orWhere('reviews', 'like', '%' . $keyword . '%');
+            }
+        }
+   
