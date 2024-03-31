3e03d90e220e1513af9fc1abff62043473b37643
Mai <mai.ho25@student.passerellesnumeriques.org>
2024-04-01 01:26:14 +0700

    Bai 35

--- hoclaravel/app/Http/Controllers/homeController.php
+++ hoclaravel/app/Http/Controllers/homeController.php
@@ -7,11 +7,22 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public $data = [];
+    protected $product; // Remove the instantiation here
+
+    public function __construct()
+    {
+        $this->product = new Product(); // Move the instantiation to the constructor
+    }
     //Action index()N
-    public function index(){ 
+    public function index(Request $request){ 
         $products = Product::all()->where('new','=',1);
         $allProducts = Product::all();
-            
+        $infor = $request->input('search');
+        
+
+        if (isset($infor)) {
+            return $this->search($infor);
+        }    
         // dd($products);
         return view('home',compact('products','allProducts'));  
     }
@@ -63,4 +74,32 @@ class HomeController extends Controller
     {
         //
     }
+    public function search($infor)
+    {
+        $query = $this->product->query();
+        $keywords = explode(' ', $infor);
+
+        foreach ($keywords as $keyword) {
+            if (is_numeric($keyword)) {
+                $query->orWhereRaw("CAST(price AS UNSIGNED) = ?", [explode('.', $keyword)[0]]);
+            } else {
+                $query->orWhere('name', 'like', '%' . $keyword . '%')
+                    ->orWhere('weight', 'like', '%' . $keyword . '%')
+                    ->orWhere('rating', 'like', '%' . $keyword . '%')
+                    ->orWhere('size', 'like', '%' . $keyword . '%')
+                    ->orWhere('reviews', 'like', '%' . $keyword . '%');
+            }
+        }
+        $data = $query->get();
+
+        // Kiểm tra xem có dữ liệu được trả về hay không
+        if ($data->isEmpty()) {
+            // Nếu không có dữ liệu, trả về view Home với thông báo "Can not found that product"
+            return view('Home', compact('data'))->with('message', 'Can not found that product');
+        }
+
+
+        // Nếu có dữ liệu, trả về view Home với dữ liệu đã tìm được
+        return view('Home', compact('data'));
+    }   
 }
