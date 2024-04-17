0fee6a6655fd02547709fe964bb5faccb8f7eef5
Mai <mai.ho25@student.passerellesnumeriques.org>
2024-04-17 14:20:38 +0700

    Complete Bai 37

--- hoclaravel/app/Http/Controllers/ProductController.php
+++ hoclaravel/app/Http/Controllers/ProductController.php
@@ -2,63 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Products;
+use App\Models\ProductType;
 use Illuminate\Http\Request;
+
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    protected $products;
+
+
+    public function __construct()
+    {
+        $this->products = new Products();
+    }
     public function index()
     {
-        //
+        $products = Products::all();
+        return view('pages/car-list', compact('products'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-        //
+        $title = "Product";
+        $product = Products::find($id);
+        return view('pages/show', ['product' => $product, 'title' => $title]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
+    public function showPricing()
     {
-        //
+        return view('pages/pricing');
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function showProductType($id)
     {
-        //
-    }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        $products = $this->products->getAllProductType($id);
+        // dd($products);
+        return view('pages/product-type', compact('products'));
     }
 }
